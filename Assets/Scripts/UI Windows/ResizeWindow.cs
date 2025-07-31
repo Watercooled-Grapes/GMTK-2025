7e5d45955e0c690622e44ea90a28c44b3148e8a0
Dorson Tang <120761661+doruphin@users.noreply.github.com>
2025-07-31 12:00:30 -0700

    resizable from all corners (#7)

--- Assets/Scripts/UI Windows/ResizeWindow.cs
+++ Assets/Scripts/UI Windows/ResizeWindow.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -6,32 +7,71 @@ public class ResizeWindow : MonoBehaviour, IDragHandler
 {
     private RectTransform windowTransform;
     private Canvas canvas;
-    private Window windowScript;
+    [SerializeField] private Window.Orientation pos;
 
     private void Awake()
     {
         if (windowTransform == null) windowTransform = transform.parent.GetComponent<RectTransform>();
         if (canvas == null) canvas = transform.parent.parent.GetComponent<Canvas>();
-        if (windowScript == null) windowScript = transform.parent.GetComponent<Window>();
 
-        this.GetComponent<Image>().color = new Color32(0,0,0,0);
+        this.GetComponent<Image>().color = new Color32(0, 0, 0, 0);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         float deltaX = eventData.delta.x / canvas.scaleFactor;
         float deltaY = eventData.delta.y / canvas.scaleFactor;
-        
+
         float currentWidth = windowTransform.rect.width;
         float currentHeight = windowTransform.rect.height;
 
-        float newWidth = currentWidth - deltaX;
-        float newHeight = currentHeight + deltaY;
+        float newWidth;
+        float newHeight;
 
-        if (newWidth < windowScript.minWidth) deltaX = 0;
-        if (newHeight < windowScript.minHeight) deltaY = 0; 
-        
-        windowTransform.offsetMin += new Vector2(deltaX, 0);
-        windowTransform.offsetMax += new Vector2(0, deltaY);
+        switch (pos)
+        {
+            case Window.Orientation.TL:
+                newWidth = currentWidth - deltaX;
+                newHeight = currentHeight + deltaY;
+
+                if (newWidth < Window.minWidth || newWidth > Window.maxWidth) deltaX = 0;
+                if (newHeight < Window.minHeight || newHeight > Window.maxHeight) deltaY = 0;
+
+                windowTransform.offsetMin += new Vector2(deltaX, 0);
+                windowTransform.offsetMax += new Vector2(0, deltaY);
+
+                break;
+            case Window.Orientation.TR:
+                newWidth = currentWidth + deltaX;
+                newHeight = currentHeight + deltaY;
+
+                if (newWidth < Window.minWidth || newWidth > Window.maxWidth) deltaX = 0;
+                if (newHeight < Window.minHeight || newHeight > Window.maxHeight) deltaY = 0;
+
+                windowTransform.offsetMax += new Vector2(deltaX, deltaY);
+
+                break;
+            case Window.Orientation.BL:
+                newWidth = currentWidth - deltaX;
+                newHeight = currentHeight - deltaY;
+
+                if (newWidth < Window.minWidth || newWidth > Window.maxWidth) deltaX = 0;
+                if (newHeight < Window.minHeight || newHeight > Window.maxHeight) deltaY = 0;
+
+                windowTransform.offsetMin += new Vector2(deltaX, deltaY);
+
+                break;
+            case Window.Orientation.BR:
+                newWidth = currentWidth + deltaX;
+                newHeight = currentHeight - deltaY;
+
+                if (newWidth < Window.minWidth || newWidth > Window.maxWidth) deltaX = 0;
+                if (newHeight < Window.minHeight || newHeight > Window.maxHeight) deltaY = 0;
+
+                windowTransform.offsetMin += new Vector2(0, deltaY);
+                windowTransform.offsetMax += new Vector2(deltaX, 0);
+
+                break;
+        }
     }
 }
--- Assets/Scripts/UI Windows/Window.cs
+++ Assets/Scripts/UI Windows/Window.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Window : MonoBehaviour
 {
-    public float minHeight = 200;
-    public float minWidth = 300;
+    public const float minHeight = 200;
+    public const float minWidth = 300;
+    public const float maxHeight = 600;
+    public const float maxWidth = 800;
+
+    public enum Orientation {TL, TR, BL, BR};
 }
