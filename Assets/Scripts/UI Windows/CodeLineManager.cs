3126dce3f248a6ee65e6097bd8951bb1aa48cd11
Dorson Tang <apersonnameddorson@gmail.com>
2025-07-30 22:30:13 -0700

    counter

---
+++ Assets/Scripts/UI Windows/CodeLineManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using Unity.Collections;
+using TMPro;
+
+public class CodeLineManager : MonoBehaviour
+{
+    private TMP_Text tmp;
+    private string[] lines;
+    private int pos = 0;
+
+    public int PLACEHOLDERCOUNTER
+    {
+        get
+        {
+            return pos;
+        }
+        set
+        {
+            pos = value;
+            updateCode();
+        }
+    } 
+
+    void Awake()
+    {
+        tmp = this.GetComponent<TMP_Text>();
+
+        lines = new string[] { "def helloWorld()", "    I like men", "    this is not a joke", "    i fuk dudes", "end" };
+
+        updateCode();
+    }
+
+    private void updateCode()
+    {
+        tmp.text = "";
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i == pos)
+            {
+                string highlightedText = lines[i].Insert(0, "<mark=#ffff00aa>");
+                highlightedText = highlightedText.Insert(lines[i].Length + "<mark=#ffff00aa>".Length, "</mark>");
+
+                tmp.text += highlightedText + "\n";
+            }
+            tmp.text += lines[i] + "\n";
+        }
+    }
+}
---
+++ Assets/Scripts/UI Windows/Window.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    public float minHeight = 200;
+    public float minWidth = 300;
+}
